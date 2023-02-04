ae0b4192730e7a4b841bf538f5ce2c2a7ecb1bd3
Gleb <dontsovggwp@mail.ru>
2023-02-04 13:32:23 +0300

    add func to main: func del and save

--- main.go
+++ main.go
@@ -42,129 +42,32 @@ func SaveQuotes(path string, mapQ map[string]map[string]string) {
 var mapQuotes = GetQuotes("quotes.json")
 var mapStar = GetQuotes("star_quotes.json")
 
-func CreateNewQuote() fyne.CanvasObject {
-	QuoteField := widget.NewMultiLineEntry()
-	QuoteField.SetPlaceHolder("Quote...")
-	QuoteField.Wrapping = fyne.TextWrapBreak
-	QuoteField.SetMinRowsVisible(10)
-
-	AuthorField := widget.NewEntry()
-	AuthorField.SetPlaceHolder("Author...")
-
-	btnSave := container.New(
-		layout.NewMaxLayout(),
-		canvas.NewRectangle(color.White),
-		widget.NewButton("Save", func() {
-			if QuoteField.Text != "" && AuthorField.Text != "" {
-				id := uuid.New().String()
-				mapQuotes[id] = map[string]string{"author": AuthorField.Text, "text": QuoteField.Text}
-				QuoteField.Text = ""
-				AuthorField.Text = ""
-				SaveQuotes("quotes.json", mapQuotes)
-			}
-		}),
-	)
-
-	box := container.NewVBox(QuoteField, AuthorField, btnSave)
-	return box
-}
-
-func CreateMyQuote() fyne.CanvasObject {
-	box := container.NewVBox()
-	mapQuotesWidget := map[string]fyne.CanvasObject{}
-
-	icStar, _ := fyne.LoadResourceFromPath("icon/star.png")
-	icShare, _ := fyne.LoadResourceFromPath("icon/share.png")
-	icDelete, _ := fyne.LoadResourceFromPath("icon/delete.png")
-
-	for i, v := range mapQuotes {
-		key := i
-		value := v
-		quote := widget.NewLabel(v["text"])
-		author := widget.NewLabel(v["author"])
-
-		quote.Wrapping = fyne.TextWrapBreak
-		author.Wrapping = fyne.TextWrapBreak
-
-		buttonStar := widget.NewButton("", func() {
-			mapStar[key] = value
-			SaveQuotes("star_quotes.json", mapStar)
-			delete(mapQuotes, key)
-			SaveQuotes("quotes.json", mapQuotes)
-			box.Objects = Remove(box.Objects, mapQuotesWidget[key])
-		})
-
-		buttonShare := widget.NewButton("", func() {})
-
-		buttonDel := widget.NewButton("", func() {
-			delete(mapQuotes, key)
-			SaveQuotes("quotes.json", mapQuotes)
-			if _, ok := mapStar[key]; !ok {
-				delete(mapStar, key)
-				SaveQuotes("star_quotes.json", mapStar)
-			}
-			box.Objects = Remove(box.Objects, mapQuotesWidget[key])
-		})
-
-		buttonStar.SetIcon(icStar)
-		buttonShare.SetIcon(icShare)
-		buttonDel.SetIcon(icDelete)
-
-		boxBtn := container.NewHBox(buttonStar, buttonShare, buttonDel)
-		text := container.NewVBox(quote, author, boxBtn)
-		card := widget.NewCard(
-			"",
-			"",
-			text,
-		)
-
-		box.Add(card)
-		mapQuotesWidget[key] = box.Objects[len(box.Objects)-1]
-	}
-	return container.NewVScroll(box)
-}
-
-func CreateFavourites() fyne.CanvasObject {
-	box := container.NewVBox()
-	icStar, _ := fyne.LoadResourceFromPath("icon/star_yel.png")
-	icShare, _ := fyne.LoadResourceFromPath("icon/share.png")
-
-	mapStarWidget := map[string]fyne.CanvasObject{}
-
-	for i, v := range mapStar {
-		key := i
-		value := v
-		quote := widget.NewLabel(v["text"])
-		author := widget.NewLabel(v["author"])
-
-		quote.Wrapping = fyne.TextWrapBreak
-		author.Wrapping = fyne.TextWrapBreak
-
-		buttonStar := widget.NewButton("", func() {
-			mapQuotes[key] = value
-			delete(mapStar, key)
-			SaveQuotes("quotes.json", mapQuotes)
-			SaveQuotes("star_quotes.json", mapStar)
-			box.Objects = Remove(box.Objects, mapStarWidget[key])
-		})
-
-		buttonShare := widget.NewButton("", func() {})
-
-		buttonStar.SetIcon(icStar)
-		buttonShare.SetIcon(icShare)
-
-		boxBtn := container.NewHBox(buttonStar, buttonShare)
-		text := container.NewVBox(quote, author, boxBtn)
-		card := widget.NewCard(
-			"",
-			"",
-			text,
-		)
-		box.Add(card)
-		mapStarWidget[key] = box.Objects[len(box.Objects)-1]
-	}
-	return container.NewVScroll(box)
-}
+//func CreateNewQuote() fyne.CanvasObject {
+//	QuoteField := widget.NewMultiLineEntry()
+//	QuoteField.SetPlaceHolder("Quote...")
+//	QuoteField.Wrapping = fyne.TextWrapBreak
+//	QuoteField.SetMinRowsVisible(10)
+//
+//	AuthorField := widget.NewEntry()
+//	AuthorField.SetPlaceHolder("Author...")
+//
+//	btnSave := container.New(
+//		layout.NewMaxLayout(),
+//		canvas.NewRectangle(color.White),
+//		widget.NewButton("Save", func() {
+//			if QuoteField.Text != "" && AuthorField.Text != "" {
+//				id := uuid.New().String()
+//				mapQuotes[id] = map[string]string{"author": AuthorField.Text, "text": QuoteField.Text}
+//				QuoteField.Text = ""
+//				AuthorField.Text = ""
+//				SaveQuotes("quotes.json", mapQuotes)
+//			}
+//		}),
+//	)
+//
+//	box := container.NewVBox(QuoteField, AuthorField, btnSave)
+//	return box
+//}
 
 func Remove(list []fyne.CanvasObject, item fyne.CanvasObject) []fyne.CanvasObject {
 	for i, value := range list {
@@ -175,20 +78,143 @@ func Remove(list []fyne.CanvasObject, item fyne.CanvasObject) []fyne.CanvasObjec
 	return list
 }
 
+func CreateCardF(key string, value map[string]string, mapWidget map[string]fyne.CanvasObject,
+	box *[]fyne.CanvasObject, box1 *[]fyne.CanvasObject,
+	icStarYel *fyne.Resource, icShare *fyne.Resource,
+	icDel *fyne.Resource, icStar *fyne.Resource) fyne.CanvasObject {
+
+	quote := widget.NewLabel(value["text"])
+	author := widget.NewLabel(value["author"])
+
+	quote.Wrapping = fyne.TextWrapBreak
+	author.Wrapping = fyne.TextWrapBreak
+
+	buttonStar := widget.NewButton("", func() {
+		mapQuotes[key] = value
+		delete(mapStar, key)
+		SaveQuotes("quotes.json", mapQuotes)
+		SaveQuotes("star_quotes.json", mapStar)
+		*box = Remove(*box, mapWidget[key])
+		*box1 = append(*box1, CreateCardM(key, value, mapWidget, box1, box, icStarYel, icShare, icDel, icStar))
+	})
+
+	buttonShare := widget.NewButton("", func() {})
+
+	buttonStar.SetIcon(*icStarYel)
+	buttonShare.SetIcon(*icShare)
+
+	boxBtn := container.NewHBox(buttonStar, buttonShare)
+	text := container.NewVBox(quote, author, boxBtn)
+	card := widget.NewCard(
+		"",
+		"",
+		text,
+	)
+	mapWidget[key] = card
+	return card
+}
+
+func CreateCardM(key string, value map[string]string, mapWidget map[string]fyne.CanvasObject,
+	box *[]fyne.CanvasObject, box1 *[]fyne.CanvasObject,
+	icStarYel *fyne.Resource, icShare *fyne.Resource,
+	icDel *fyne.Resource, icStar *fyne.Resource) fyne.CanvasObject {
+
+	quote := widget.NewLabel(value["text"])
+	author := widget.NewLabel(value["author"])
+
+	quote.Wrapping = fyne.TextWrapBreak
+	author.Wrapping = fyne.TextWrapBreak
+
+	buttonStar := widget.NewButton("", func() {
+		mapStar[key] = value
+		delete(mapQuotes, key)
+		SaveQuotes("quotes.json", mapQuotes)
+		SaveQuotes("star_quotes.json", mapStar)
+		*box = Remove(*box, mapWidget[key])
+		*box1 = append(*box1, CreateCardF(key, value, mapWidget, box1, box, icStarYel, icShare, icDel, icStar))
+	})
+
+	buttonDel := widget.NewButton("", func() {
+		delete(mapQuotes, key)
+		SaveQuotes("quotes.json", mapQuotes)
+		if _, ok := mapStar[key]; !ok {
+			delete(mapStar, key)
+			SaveQuotes("star_quotes.json", mapStar)
+		}
+		*box1 = Remove(*box1, mapWidget[key])
+		delete(mapWidget, key)
+	})
+
+	buttonShare := widget.NewButton("", func() {})
+
+	buttonStar.SetIcon(*icStar)
+	buttonShare.SetIcon(*icShare)
+	buttonDel.SetIcon(*icDel)
+
+	boxBtn := container.NewHBox(buttonStar, buttonShare, buttonDel)
+	text := container.NewVBox(quote, author, boxBtn)
+	card := widget.NewCard(
+		"",
+		"",
+		text,
+	)
+	mapWidget[key] = card
+	return card
+}
+
 func main() {
 	a := app.New()
 	//a.Settings().SetTheme(theme.DarkTheme())
 	w := a.NewWindow("MyApp")
 	w.Resize(fyne.NewSize(380, 670))
 
-	formNewQuote := CreateNewQuote()
-	formMyQuotes := CreateMyQuote()
-	formFavourites := CreateFavourites()
+	icStar, _ := fyne.LoadResourceFromPath("icon/star.png")
+	icStarYel, _ := fyne.LoadResourceFromPath("icon/star_yel.png")
+	icShare, _ := fyne.LoadResourceFromPath("icon/share.png")
+	icDelete, _ := fyne.LoadResourceFromPath("icon/delete.png")
+
+	box := container.NewVBox()
+	box1 := container.NewVBox()
+
+	mapWidget := map[string]fyne.CanvasObject{}
+
+	for i, v := range mapStar {
+		card := CreateCardF(i, v, mapWidget, &box.Objects, &box1.Objects, &icStarYel, &icShare, &icDelete, &icStar)
+		box.Add(card)
+	}
+
+	for i, v := range mapQuotes {
+		card := CreateCardM(i, v, mapWidget, &box1.Objects, &box.Objects, &icStarYel, &icShare, &icDelete, &icStar)
+		box1.Add(card)
+	}
+
+	QuoteField := widget.NewMultiLineEntry()
+	AuthorField := widget.NewEntry()
+	QuoteField.SetPlaceHolder("Quote...")
+	QuoteField.SetMinRowsVisible(10)
+	AuthorField.SetPlaceHolder("Author...")
+	QuoteField.Wrapping = fyne.TextWrapBreak
+	btnSave := container.New(
+		layout.NewMaxLayout(),
+		canvas.NewRectangle(color.White),
+		widget.NewButton("Save", func() {
+			if QuoteField.Text != "" && AuthorField.Text != "" {
+				id := uuid.New().String()
+				mapQuotes[id] = map[string]string{"author": AuthorField.Text, "text": QuoteField.Text}
+				QuoteField.Text = ""
+				AuthorField.Text = ""
+				SaveQuotes("quotes.json", mapQuotes)
+				box1.Add(CreateCardM(id, mapQuotes[id], mapWidget, &box1.Objects,
+					&box.Objects, &icStarYel, &icShare, &icDelete, &icStar))
+			}
+		}),
+	)
+	box2 := container.NewVBox(QuoteField, AuthorField, btnSave)
 
 	tabs := container.NewAppTabs(
-		container.NewTabItem("My quotes", formMyQuotes),
-		container.NewTabItem("New quote", formNewQuote),
-		container.NewTabItem("Favourites", formFavourites),
+		container.NewTabItem("My quotes", container.NewVScroll(box1)),
+		container.NewTabItem("New quote", box2),
+		container.NewTabItem("Favourites", container.NewVScroll(box)),
 		//container.NewTabItem("Book", widget.NewLabel("Books")),
 	)
 	tabs.SetTabLocation(container.TabLocationBottom)
